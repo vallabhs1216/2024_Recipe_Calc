e09062df77487b708d83d62346b8796c14dd333c
vallabhs1216 <100321435+vallabhs1216@users.noreply.github.com>
2024-07-03 14:47:43 +1200

    Added Instructions, and fixed some comments.

--- B0_Recipe_Calc_V4.py
+++ B0_Recipe_Calc_V4.py
@@ -6,7 +6,23 @@ import pandas
 
 # instructions go here
 def show_instructions():
-    print('')
+    print("🍕Welcome to the Recipe Calculator🍕")
+    print()
+    print("Please start by entering your recipe name and amount of servings it makes.")
+    print("And the total amount of ingredients you are using.")
+    print()
+    print("You can then start entering information about each ingredient,")
+    print("You will be asked:")
+    print()
+    print("Ingredients name,")
+    print()
+    print("The unit it was bought in (Kg, g, mL, L), ")
+    print("and the amount of the ingredient bought,")
+    print()
+    print("The unit of how much you used (Kg, g, mL, L),")
+    print("and the amount of the ingredient used.")
+    print()
+    print("How much it cost to buy the ingredient.")
 
 
 # checks that input is either a float or an
@@ -59,11 +75,11 @@ def currency(x):
 
 # Main routine goes here
 
-# list
+# List
 yes_no_list = ["yes", "no"]
 units_list = ["kg", "grams", "litres", "ml", "kilograms", "millilitres"]
 
-# lists to hold ingredient details
+# Lists to hold ingredient details
 ingredients_list = []
 amount_list = []
 unit_bought_list = []
@@ -72,7 +88,7 @@ unit_used_list = []
 cost_list = []
 cost_to_make_list = []
 
-# Dictionary used to create data frame ie: column_name:list
+# Dictionary used to create data frame
 recipe_cost_dict = {
     "Ingredients": ingredients_list,
     "Amount": amount_list,
@@ -83,16 +99,18 @@ recipe_cost_dict = {
     "Cost To Make": cost_to_make_list
 }
 
-# set maximum number of ingredients below
-MAX_INGREDIENTS = 99
+# Ask user if the want instructions
+Instructions = string_checker("Do you want to see the instructions? ", 1, yes_no_list, "Please enter yes or no. ")
+
+# Set maximum number of ingredients below
+MAX_INGREDIENTS = num_check("How many ingredients are you using? ", "Please enter a number more than 0\n", int)
 ingredients_listed = 0
 
-# get recipe name
+# Get recipe name
 recipe_name = not_blank("Recipe name: ", "The recipe name can't be blank.")
-filename = recipe_name
 
 servings = num_check("How many servings does this recipe make? ", "Please enter a number more than 0\n", int)
-# Loop to get information for table
+# Loop to get information for list
 
 while ingredients_listed < MAX_INGREDIENTS:
     # Get ingredient names
@@ -163,6 +181,10 @@ Servings = f"*** Servings: {servings} ***\n"
 total = f"\nTotal Cost: ${total:.2f}\n"
 per_serve = f"Cost Per Serve: ${per_serve:.2f}"
 
+# Ask user for file name
+filename = input("Please enter a file name: ")
+if filename == "":
+    filename = recipe_name
 
 to_write = [Heading, Servings, ingredient_string, total, per_serve]
 
@@ -170,7 +192,7 @@ to_write = [Heading, Servings, ingredient_string, total, per_serve]
 for item in to_write:
     print(item)
 
-# write output to file
+# Write output to file
 # Create file to hold data (add .txt extension)
 write_to = f"{filename}.txt"
 text_file = open(write_to, "w+")
